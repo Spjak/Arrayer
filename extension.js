96b5f8dd6e110d616409a04526b73da5c885c8b9
Jacob <spjak1@gmail.com>
2022-01-16 10:53:43 +0100

    Improved removal of separators

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@ const QUOTE = '"'
 const SEPARATOR = ','
 const BRACKET_START = '['
 const BRACKET_END = ']'
+const PATTERN_SEARCH_LINE_COUNT = 2
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -20,29 +21,29 @@ function activate(context) {
 		lines = lines.map(l => l.trim())
 		console.table(lines)
 		// Look for patterns
-		let patternLines = lines.slice(0,Math.min(lines.length,4))
+		let patternLines = lines.slice(0,Math.min(lines.length, PATTERN_SEARCH_LINE_COUNT))
 	    // Find extra separator characters
 		let startSeparatorCount = 0
 		let endSeparatorCount = 0
 		for (let p of patternLines) {
-			if (isSeparator(p.substring(0,1)))
+			if (isSeparator(p.substring(0, 1)))
 			{
 				startSeparatorCount++
 			}
-			if (isSeparator(p.substring(p.length-1,p.length)))
+			if (isSeparator(p.substring(p.length - 1, p.length)))
 			{
 				endSeparatorCount++
 			}
 		}
 		// Remove extra separator characters
 		if (startSeparatorCount == patternLines.length) {
-			lines = lines.map(l => l.substring(1))
+			lines = lines.map(l => removeStartSeparator(l))
 		}
 		if (endSeparatorCount == patternLines.length) {
-			lines = lines.map(l => l.substring(0,l.length-1))
+			lines = lines.map(l => removeEndSeparator(l))
 		}
 		// Detect if lines are numbers, to allow unquoted list
-		patternLines = lines.slice(0,Math.min(lines.length,4))
+		patternLines = lines.slice(0,Math.min(lines.length, PATTERN_SEARCH_LINE_COUNT))
 		let isNumbers = true
 		for (let p of patternLines) {
 			if (!isNumber(p))
@@ -59,10 +60,10 @@ function activate(context) {
 		else {
 			lines.forEach(l => outputString += `${QUOTE}${l}${QUOTE}${SEPARATOR}`)
 		}
-		outputString = outputString.substring(0,outputString.length-1)
+		outputString = outputString.substring(0, outputString.length - 1)
 		outputString += BRACKET_END
 		editor.edit((editBuilder) => {
-			editBuilder.replace(editor.selection,outputString)
+			editBuilder.replace(editor.selection, outputString)
 		})  
 		vscode.window.showInformationMessage(`${outputString}`)
 	});
@@ -73,22 +74,24 @@ function activate(context) {
 function deactivate() {}
 
 function isSeparator(s) {
-    separatorRegex = /[,;]/
+    let separatorRegex = /[,;]/
     return s.match(separatorRegex)
 }
 
-function isInteger(s) {
-    integerRegex = /^(-?)[0-9]+$/
-    return s.match(integerRegex)
+function removeStartSeparator(s) {
+	let separatorRegex = /^([,;])(.*)/
+	let res = s.match(separatorRegex)
+	return res ? res[2] : s
 }
 
-function iDecimal(s) {
-    decimalRegex = /^(-?)([0-9]+)[\.,]([0-9]+)$/
-    return s.match(decimalRegex)
+function removeEndSeparator(s) {
+	let separatorRegex = /(.*)([,;])$/
+	let res = s.match(separatorRegex)
+	return res ? res[1] : s
 }
 
 function isNumber(s) {
-    numberRegex = /^(-?)([0-9]+)[\.,]?([0-9]+)$/
+    let numberRegex = /^(-?)([0-9]+)[\.]?([0-9]+)$/
     return s.match(numberRegex)
 }
 
